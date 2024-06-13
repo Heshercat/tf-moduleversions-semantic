5f725890047c2e8c358cb26bb2c171a795095486
Heshercat <alfiiaqa@gmail.com>
2024-06-13 12:54:07 +0300

    Add valid tf plan

--- semantic_versions.tf
+++ semantic_versions.tf
@@ -1,31 +1,67 @@
-provider "null" {}
+# variables.tf
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+  default     = "example_project"
+}
 
-resource "null_resource" "color_message" {
-  count = 6
+variable "instance_count" {
+  description = "Number of instances"
+  type        = number
+  default     = 1
+}
+
+variable "tags" {
+  description = "A map of tags to assign to resources"
+  type        = map(string)
+  default     = {
+    "Environment" = "dev"
+    "Owner"       = "team"
+  }
+}
+
+# main.tf
+resource "aws_instance" "example" {
+  count = var.instance_count
+
+  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI ID
+  instance_type = "t2.micro"
+
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "${var.project_name}-instance-${count.index}"
+    }
+  )
+
+  # Trigger to recreate resource if time changes
+  lifecycle {
+    create_before_destroy = true
+  }
 
   provisioner "local-exec" {
-    command = <<-EOT
-      case ${count.index} in
-        0)
-          color="31"  # Red
-          ;;
-        1)
-          color="32"  # Green
-          ;;
-        2)
-          color="33"  # Yellow
-          ;;
-        3)
-          color="34"  # Blue
-          ;;
-        4)
-          color="35"  # Magenta
-          ;;
-        5)
-          color="36"  # Cyan
-          ;;
-      esac
-      echo -e "\e[${color}mThis is \e[${color}m message\e[0m"
-    EOT
+    command = "echo The time is ${timestamp()}"
   }
+}
+
+# outputs.tf
+output "project_name" {
+  description = "The name of the project"
+  value       = var.project_name
+}
+
+output "instance_count" {
+  description = "The number of instances"
+  value       = var.instance_count
+}
+
+output "tags" {
+  description = "The tags assigned to resources"
+  value       = var.tags
+}
+
+# locals.tf
+locals {
+  # Local time to trigger resource recreation
+  current_time = timestamp()
 }
